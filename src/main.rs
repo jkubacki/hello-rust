c5854080f18e14c9c937b9f5c7124ceca3757bcb
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-05 20:38:06 +0200

    Touples

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,9 @@ fn main() {
     let x: i32;
     x = 512;
     println!("Hello, world! x = {}", x);
+
+    let touple: (i32, i32, i32) = (1, 2, 3);
+    let (a, b, c) = touple;
+
+    println!("a = {}, b = {}, c = {}", a, b, c);
 }
