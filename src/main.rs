396520fa4e1ad7e640ed925b19d5aa47cf7429bd
Jakub Kubacki <kubacki.jk@gmail.com>
2024-06-05 20:42:21 +0200

    Functions and blocks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+fn add(a: i32, b: i32) -> i32 {
+    // return a + b;
+    a + b // ommit semi-colon for tail to return
+}
+
 fn main() {
     // variable
     let x: i32;
@@ -9,4 +14,17 @@ fn main() {
     let (a, b, _c) = touple;
 
     println!("a = {}, b = {}", a, b);
+
+    // function
+    let result = add(1, 2);
+    println!("result = {}", result);
+
+    // block like in ruby
+    let block = {
+        let a = 1;
+        let b = 2;
+        a + b // tail
+    };
+
+    println!("block = {}", block);
 }
